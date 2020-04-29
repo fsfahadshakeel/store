5464853c2be03ea417f563e258a0201bff3a3ae0
Fahad Shakeel <fsfahadshakeel@gmail.com>
2020-04-29 13:55:37 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductsList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Error from './components/Error';
+import Modal from './components/Modal';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={ProductList}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route component={Error}></Route>
+      </Switch>
+      <Modal/>
+    </>
+  );
+}
+
+export default App;
